408145c7c73604b93645b8be733365d39279af28
vekajp <belka.v.2002@gmail.com>
2022-06-07 16:07:33 +0300

    cleanup and add curve of sum

--- graphing.py
+++ graphing.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-def make_graph(x, c1, c2):
+def make_graph(x, c1, c2, title=""):
     c = []
     c1m = []
     c2m = []
+    c_sum = []
     for i in range(0, len(x)):
         c.append(abs(c2[i]) ** 2 - abs(c1[i]) ** 2)
         c1m.append(abs(c1[i]) ** 2)
         c2m.append(abs(c2[i]) ** 2)
+        c_sum.append(abs(c2[i]) ** 2 + abs(c1[i]) ** 2)
 
-    print(c1)
-    print(c2)
-    print(c)
     plt.plot(x, c, label="|C2|^2 - |C1|^2")
+    plt.plot(x, c_sum, label="|C2|^2 + |C1|^2")
     plt.plot(x, c1m, label="|C1|^2")
     plt.plot(x, c2m, label="|C2|^2")
     plt.legend()
+    plt.title(title)
     plt.show()
